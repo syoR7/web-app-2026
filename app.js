59d0aa06f8e7af165a183ada9a741f157c937dec
NK-syo <xiangdalitian058@gmail.com>
2026-05-13 13:08:47 +0900

    mainで作業

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+//ないと読み込まれない
+app.use(express.json());
+app.use(express.static('public'));
 //ルート１ :トップページ
 app.get('/',(req, res) => {
     res.send('トップページです');
@@ -13,6 +16,26 @@ app.get('/about', (req,res) => {
 app.get('/time' , (req, res)=> {
     const now = new Date().toLocaleString('ja-JP');
     res.send('現在時刻:' + now);
+});
+
+app.get('/api/test',(req, res) => {
+    res.json({message: 'APIが動いています',status : 'ok'});
+})
+app.get('/status', (req, res) => {
+    res.json({status: 'ok', message:'サーバーは正常に動作しています'});
+});
+const messages = [];
+//GET：メゾット一覧を取得
+app.get('/api/messages', (req,res) => {
+    res.json(messages);
+});
+
+//POST:メッセージを追加
+app.post('/api/messages', (req,res) =>{
+    const {username, text}= req.body;
+    const newMessage = {id: messages.length + 1, username, text};
+    messages.push(newMessage);
+    res.json(newMessage);
 });
 app.listen(3000, () => {
     console.log('サーバーが起動しました： http://localhost:3000');
